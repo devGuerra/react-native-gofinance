c4fed7941062ab34163ba9d4adc3f7a020521bae
Roberto Guerra <prosaka@hotmail.com>
2021-09-15 18:04:55 -0300

    aula 3 modulo 2

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { ThemeProvider } from "styled-components";
+
+import { Dashboard } from "./src/screens/Dashboard";
+import theme from "./src/global/Styles/theme";
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
