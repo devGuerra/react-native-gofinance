c3d12ed0286b0abf7b5507481ca3ef594125cf34
Roberto Guerra <prosaka@hotmail.com>
2021-09-15 21:50:08 -0300

    cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,58 @@
 import React from "react";
+import { HighlightCard } from "../../Components/HighlightCard";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/6579736?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Roberto</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de setembro"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última entrada dia 13 de setembro"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="Última entrada dia 13 de setembro"
+          type="total"
+        />
+      </HighlightCards>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,66 @@
 import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: ${({theme}) => theme.colors.background};
-`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
 
-export const Title = styled.Text`
-    font-size: 24px;
-    font-weight: bold;
-    color: ${({theme}) => theme.colors.title}
-`
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 ${RFValue(24)}px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)};
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: ${RFValue(17)}px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: RFValue(24) },
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
