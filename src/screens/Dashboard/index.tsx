d6263b41a4344328a59f13d1518498b18a3cd73a
Roberto Guerra <prosaka@hotmail.com>
2021-09-15 22:33:53 -0300

    checkpoint

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+
 import { HighlightCard } from "../../Components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../Components/TransactionCard";
 
 import {
   Container,
@@ -12,9 +17,43 @@ import {
   UserName,
   Icon,
   HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
 } from "./styles";
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      category: { name: "Vendas", icon: "dollar-sign" },
+      date: "01/03/2021",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hambugueria Pizzy",
+      amount: "R$ 59,00",
+      category: { name: "Alimentação", icon: "coffee" },
+      date: "12/06/2021",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200,00",
+      category: { name: "Casa", icon: "shopping-bag" },
+      date: "20/07/2021",
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -53,6 +92,16 @@ export function Dashboard() {
           type="total"
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 }
